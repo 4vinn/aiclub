3a71356788fd397f2757376348b3ef352529d671
4vinn <vineet.iitdelhi@gmail.com>
2025-02-02 04:53:54 +0530

    new church hero section 2.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Section1 from "./components/section1";
-import Section2 from "./components/section2";
+import Section1 from "./components/Section1";
+import Section2 from "./components/Section2";
 
 function App() {
   return (
