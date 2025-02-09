b45e3f7a014191050d026d14f4d1804e0c7d8cbc
4vinn <vineet.iitdelhi@gmail.com>
2025-02-09 21:00:05 +0530

    Navbar update + dark theme contact page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,53 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+let tabs = [
+  { id: "home", label: "Home", to: "" },
+  { id: "contact", label: "Contact", to: "/contact" },
+];
 
 function Navbar() {
   const location = useLocation();
-  const isContactPage = location.pathname === "/contact";
-  const isHomePage = location.pathname === "/";
+  // const activeTab = tabs.find((tab) => tab.to === location.pathname)?.id || "home";
+  const activeTab = location.pathname === "/" ? "home" : "contact";
   return (
-    <nav
-      className={`fixed top-6 left-1/2 transform -translate-x-1/2 w-auto px-6 py-1 flex items-center justify-center
-    ${isContactPage ? "bg-[#fff] text-black" : "bg-black/50 text-white"} 
-    backdrop-blur-lg rounded-full flex items-center space-x-3 shadow-lg 
-    border-[1px] ${
-      isContactPage ? "border-gray-300" : "border-[#5b5b5b]"
-    } z-50`}
-    >
+    <nav className="fixed top-6 left-1/2 transform -translate-x-1/2 w-auto px-6 py-1 flex items-center justify-center bg-black/50 text-white backdrop-blur-lg rounded-full  space-x-3 shadow-lg border-[1px] border-[#5b5b5b] z-50">
       {/* Logo */}
       <div className="flex items-center">
         <img
           src="./photos/logo-nobg.png"
           alt="myaiclub logo"
           width={90}
-          className={`min-w-[90px] ${isContactPage ? "invert" : ""}`}
+          className="min-w-[90px]"
         />
       </div>
 
       {/* Navigation Links */}
-      <div className="flex text-lg font-inter font-normal">
-        <Link
-          to="/"
-          className={` transition px-4 py-1 rounded-full ${
-            isHomePage
-              ? "bg-[#767676]/30 hover:text-gray-400"
-              : "hover:text-gray-400"
-          }`}
-        >
-          Home
-        </Link>
-        <Link
-          to="/contact"
-          className={` transition px-4 py-1 rounded-full ${
-            isContactPage
-              ? "bg-[#efefef] hover:text-gray-400"
-              : "hover:text-gray-400"
-          }`}
-        >
-          Contact
-        </Link>
+      <div className="flex">
+        {tabs.map((tab) => (
+          <Link
+            key={tab.id}
+            to={tab.to}
+            // onClick={() => setActiveTab(tab.id)}
+            className={`${
+              activeTab === tab.id ? "" : "hover:text-white/60"
+            } relative rounded-full  px-4 py-1 text-lg font-inter font-normal text-white transition `}
+            style={{
+              WebkitTapHighlightColor: "transparent",
+            }}
+          >
+            {activeTab === tab.id && (
+              <motion.span
+                layoutId="bubble"
+                className="absolute inset-0 z-10 bg-[#767676]/40 mix-blend-difference rounded-full "
+                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+              />
+            )}
+            {tab.label}
+          </Link>
+        ))}
       </div>
     </nav>
   );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,27 +1,43 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { Calendar, Linkedin, Mail } from "lucide-react";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 
-const ContactCard = ({ icon: Icon, title, description, link, delay }) => (
-  <motion.a
-    href={link}
-    target="_blank"
-    rel="noopener noreferrer"
-    initial={{ opacity: 0, y: 0 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ delay, duration: 0.8 }}
-    className="group p-8 rounded-2xl bg-white border border-gray-100 shadow-sm hover:shadow-md transition-all duration-300 flex flex-col items-center text-center"
-  >
-    <div className="w-12 h-12 rounded-full bg-[#f4f4f4] flex items-center justify-center mb-4 group-hover:bg-[#e4e4e4] transition-colors">
-      <Icon className="w-6 h-6 text-gray-600" strokeWidth={1.2} />
-    </div>
-    <h3 className="text-xl font-medium text-gray-900 mb-2">{title}</h3>
-    <p className="text-gray-600 text-base">{description}</p>
-  </motion.a>
-);
+const CONTACT_CARDS = [
+  {
+    icon: Calendar,
+    title: "Events & Meetups",
+    description: "Watch out for upcoming events and networking opportunities",
+    link: "https://lu.ma/myaiclub",
+    delay: 0.2,
+  },
+  {
+    icon: Linkedin,
+    title: "LinkedIn",
+    description: "Connect with us and grow your professional network",
+    link: "https://www.linkedin.com/company/myaiclub/",
+    delay: 0.35,
+  },
+  {
+    icon: Mail,
+    title: "Email",
+    description: "Have questions? Reach out to our team directly",
+    link: "mailto:team@myaiclub.com",
+    delay: 0.5,
+  },
+];
 
 const Contact = () => {
+  let mouseX = useMotionValue(0);
+  let mouseY = useMotionValue(0);
+
+  function handleMouseMove(event) {
+    let { left, top } = event.currentTarget.getBoundingClientRect();
+
+    mouseX.set(event.clientX - left);
+    mouseY.set(event.clientY - top);
+  }
   return (
-    <div className="min-h-screen md:h-screen items-center justify-center flex md:pt-16 pb-40 px-10 md:px-4 font-inter bg-[#f7f7f7]">
+    <div className="min-h-screen md:h-screen items-center justify-center flex md:pt-16 pb-40 px-10 md:px-4 font-inter bg-[#121212]">
       <div className="max-w-4xl mx-auto pt-20 pb-16 ">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
@@ -29,36 +45,58 @@ const Contact = () => {
           transition={{ duration: 0.8 }}
           className="text-center mb-16"
         >
-          <h1 className="mt-6 text-4xl font-semibold text-gray-900 font-poppins">
+          <h1 className="mt-6 text-4xl font-semibold text-white font-poppins">
             Stay Connected
           </h1>
-          <p className="mt-4 text-xl text-gray-600 font-poppins">
+          <p className="mt-4 text-xl text-[#808080] font-poppins">
             Join our community and never miss an update.
           </p>
         </motion.div>
 
-        <div className="grid gap-4 md:grid-cols-3">
-          <ContactCard
-            icon={Calendar}
-            title="Events & Meetups"
-            description="Watch out for upcoming events and networking opportunities"
-            link="https://lu.ma/myaiclub"
-            delay={0.2}
-          />
-          <ContactCard
-            icon={Linkedin}
-            title="LinkedIn"
-            description="Connect with us and grow your professional network"
-            link="https://www.linkedin.com/company/myaiclub/"
-            delay={0.4}
-          />
-          <ContactCard
-            icon={Mail}
-            title="Email"
-            description="Have questions? Reach out to our team directly"
-            link="mailto:team@myaiclub.com"
-            delay={0.6}
-          />
+        <div className="grid gap-6 md:grid-cols-3">
+          {CONTACT_CARDS.map((card) => (
+            <motion.div
+              initial={{ opacity: 0, y: 20, x: 10 }}
+              animate={{ opacity: 1, y: 0, x: 0 }}
+              transition={{ delay: card.delay, duration: 0.8, ease: "easeOut" }}
+              className="group relative max-w-md rounded-xl border border-white/10 bg-[#121212] shadow-2xl"
+              onMouseMove={handleMouseMove}
+            >
+              {/* glow */}
+              <motion.div
+                className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition-all duration-500 group-hover:opacity-100"
+                style={{
+                  background: useMotionTemplate`
+              radial-gradient(
+                480px circle at ${mouseX}px ${mouseY}px,
+                rgba(218, 231, 237, 0.15),
+                transparent 99%
+              )
+            `,
+                }}
+              />
+              <a
+                key={card.title}
+                href={card.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group flex flex-col items-center text-center px-8 py-10 "
+              >
+                <div className="w-12 h-12 rounded-full bg-[#1e1e1e] flex items-center justify-center mb-8 group-hover:bg-[#3d3d3d] transition-all duration-300">
+                  <card.icon
+                    className="w-6 h-6 text-gray-200"
+                    strokeWidth={1.2}
+                  />
+                </div>
+                <h3 className="text-xl font-medium text-white mb-2">
+                  {card.title}
+                </h3>
+                <p className="text-[#808080] group-hover:text-white text-base transition-all duration-300">
+                  {card.description}
+                </p>
+              </a>
+            </motion.div>
+          ))}
         </div>
       </div>
     </div>
